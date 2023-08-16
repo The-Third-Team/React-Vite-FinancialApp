7a63d27ecd730d1aa349e7a76e338dc7811539c0
yutaokkots <yuta.okkotsu@gmail.com>
2023-08-16 14:36:13 -0600

    updated budgetOverview page

--- src/pages/BudgetOverviewPage/BudgetOverviewPage.jsx
+++ src/pages/BudgetOverviewPage/BudgetOverviewPage.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import Navbar from '../../components/NavBar/Navbar'
+import PageHeader from '../../components/PageHeader/PageHeader'
+
+
 
 export default function BudgetOverviewPage() {
+
+
   return (
-    <div>BudgetOverviewPage</div>
+    <>
+      <Navbar/>
+      <div className='ml-[15vw] w-[85vw]'>
+        <div className='flex flex-col items-center w-[85vw]'>
+          <PageHeader>YOUR BUDGET</PageHeader>
+            <div 
+              className="border-2 border-gray-100 rounded-md p-10"
+                >Flowbite pie chart here</div>
+
+            <div
+                className="flex"
+                >Income</div>
+            <div
+              className="border-2 border-gray-100 rounded-md p-5"
+              >
+                <div
+                  className="flex flex-row justify-between ">
+                    <div
+                        className="text-gray-300 text-xs">
+                        MEADE PUBLICATIONS*MODE</div>
+                    <div
+                      className="text-xl font-bold"
+                      >$1,234</div>
+                </div>
+            </div>
+
+
+            <div
+              className="grid grid-cols-6 justify-between">
+                <div className="col-span-2">Expenses</div>
+                <div className="col-span-2">Budget</div>
+                <div className="col-span-1">Remaining</div>
+            </div>
+
+        </div>
+
+
+      </div>
+
+    </>
   )
 }
 
