e1eaa8fc657c6931a1df48e9233967f2c595a9c9
Scynes <dustin.greyfield@gmail.com>
2023-08-16 23:30:46 -0400

    wip/chore(EditField): added a couple dependencies for react icons and flowbite, added functionality and interfacing to EditField component

--- src/components/EditField/EditField.tsx
+++ src/components/EditField/EditField.tsx
@@ -1,8 +1,50 @@
-import React from "react";
+import { Button, Modal } from "flowbite-react";
+import React, { useState } from "react";
 
-const EditField = () => {
+import { MdModeEditOutline } from 'react-icons/md';
+
+interface Expense {
+    title: string,
+    amount: string
+}
+
+interface Properties {
+    // The category title
+    category: string,
+    // The data array of Expense objects
+    data: Expense[],
+    // Callback method for when saving
+    onSave: () => void
+}
+
+const EditField = ({ category, data, onSave }: Properties) => {
+
+    const [ openModal, setOpenModal ] = useState<string | undefined>(); 
+
+    const props = { openModal, setOpenModal };
+
     return (
         <>
+        <Button onClick={ () => props.setOpenModal('pop-up') } className={'w-[2rem] border-none text-black'}>
+            <MdModeEditOutline size={ 20 } />
+        </Button>
+        <Modal className={ 'bg-indigo-950/50' } show={props.openModal === 'pop-up'} size="sm" popup onClose={() => props.setOpenModal(undefined)}>
+            <Modal.Header />
+            <Modal.Body className={ 'max-w-sm p-6 pt-0 bg-white rounded-lg' }>
+                <h3 className={ 'mb-5 text-lg text-center font-normal text-gray-500 dark:text-gray-400' }>{ category }</h3>
+                <div className="max-h-[15rem] overflow-auto flex flex-col justify-center gap-4">
+                    { data.map((expense, index) => (
+                        <div className={ 'flex justify-between w-full' } key={ index }>
+                            <p className={ 'flex items-center' }>{ expense.title }</p>
+                            <div>
+                                <input className={ 'max-w-[4rem] rounded text-center' } type="text" placeholder={ expense.amount } />
+                            </div>
+                        </div>
+                    )) }
+                </div>
+                <Button className={ 'w-full mt-5 bg-red-500' }>Save</Button>
+            </Modal.Body>
+        </Modal>
         </>
     );
 }
