fffa1cfbca61961348f536697d6111e4358eecec
Vilem-McKael <vilemckael@gmail.com>
2023-08-18 02:44:03 -0700

    responsive design for laptop

--- src/components/AccountWidget/AccountWidget.jsx
+++ src/components/AccountWidget/AccountWidget.jsx
@@ -5,7 +5,7 @@ export default function AccountWidget({name, linkTo, imgSrc}) {
   return (
     <div className='flex flex-col items-center w-full'>
     
-      <Link to={linkTo} className='flex flex-col justify-center items-center w-[50%] h-[60px] bg-white mx-2 mt-2 border-black border-[1px] rounded-[10px]'>
+      <Link to={linkTo} className='flex flex-col justify-center items-center w-[60px] h-[60px] bg-white mx-2 mt-2 border-black border-[1px] rounded-[10px]'>
         <img src={imgSrc} className='w-[100%]'/>
       </Link>
       <div className='text-center text-[12px] mb-2 mt-1'>
