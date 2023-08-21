5a1b9457e71761c5e27f2d0913937db7daf41734
Vilem-McKael <vilemckael@gmail.com>
2023-08-21 15:22:55 -0700

    changes target api address to heroku backend

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://newleafbe-d8d834bde5db.herokuapp.com/',
         changeOrigin: true,
       },
     }
