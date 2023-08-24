c60a56b90d2d54a19f67d5bc64347b4c93d08db4
yutaokkots <yuta.okkotsu@gmail.com>
2023-08-23 23:33:49 -0600

    updated vite.config to include vercel env

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,26 @@
-// import { defineConfig, loadEnv } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// export default ({ mode }) => {
-//     process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+export default ({ mode }) => {
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
 
-//     return defineConfig({
-//         plugins: [react()],
-//         server: {
-//           proxy: {
-//             '/api': {
-//               target: process.env.VITE_BACKEND_URL ,
-//               changeOrigin: true,
-//             },
-//           }
-//         }
-//     });
-// }
+    return defineConfig({
+        plugins: [react()],
+        server: {
+          proxy: {
+            '/api': {
+              target: process.env.VITE_VERCEL_ENV,
+              changeOrigin: true,
+            },
+          }
+        }
+    });
+}
 
-export default defineConfig(() => {
-  return {
-    define: {
-      __APP_ENV__: process.env.VITE_VERCEL_ENV,
-    },
-  };
-});
+// export default defineConfig(() => {
+//   return {
+//     define: {
+//       __APP_ENV__: process.env.VITE_VERCEL_ENV,
+//     },
+//   };
+// });
